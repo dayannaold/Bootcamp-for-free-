7664b6c6eaf467da84aa7e9d079c2912c47beea5
dayannaold <180118889@aluno.unb.br>
2023-02-21 21:53:56 -0800

    render

--- Snack Game/game.js
+++ Snack Game/game.js
@@ -1,12 +1,27 @@
+import { updtae as updateSnake, draw as drawSnake, SNAKE_SPEED} from './snake.js'
+
 let lastRenderTime = 0;
-const SNAKE_SPEED = 2;
+const gameBoard = document.getElementsById ('game-board')
+
 function main(currentTime) {
     window.requestAnimationFrame(main);
     const secondSinceLastRender = (currentTime - lastRenderTime) / 1000;
-    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
-   console.log('Render'); 
+    if (secondSinceLastRender < 1 / SNAKE_SPEED) return;
+    
     lastRenderTime = currentTime;
+
+    update()
+    draw()
     
 }
 
 window.requestAnimationFrame(main);
+
+function update(){
+    updateSnake()
+
+}
+function draw(){
+    drawSnake(gameBoard)
+
+}
