d1d40bd1b4401cc3f65dee6e3ea2f07890e07c13
dayannaold <180118889@aluno.unb.br>
2023-02-22 22:14:10 -0800

    snake food created

--- Snack Game/snake.js
+++ Snack Game/snake.js
@@ -1,22 +1,25 @@
-export const SNAKE_SPEED = 1
-const snakeBody = [
-    { x: 10, y: 11},
-    { x: 11, y: 11},
-    { x: 12, y: 11},
-]
+import { getInputDirection } from "./input.js"
+
+export const SNAKE_SPEED = 5
+const snakeBody = [{ x: 11, y: 11}]
 
 export function update() {
-    console.log ('update snake')
+   const inputDirection = getInputDirection()
+    for (let i = snakeBody.length - 2; i >= 0; i-- ){
+        snakeBody[i +i] = { ...snakeBody[i] }
+    }
+
+    snakeBody[0].x += inputDirection.x
+    snakeBody[0].y += inputDirection.y
 }
 
-export function draw(gameBoard){
+export function draw(gameBoard) {
     snakeBody.forEach(segment => {
-        const snakeElement = document.createElement('div')
-        snakeElement.style.gridRowStart = segment.x
-        snakeElement.style.gridColumnStart = segment.y
-        snakeElement.classList.add('snake')
+        const snakeElement = document.createElement("div");
+        snakeElement.style.gridRowStart=segment.y
+        snakeElement.style.gridColumnStart=segment.y
+        snakeElement.classList.add("snake")
         gameBoard.appendChild(snakeElement)
     })
-}
-// rendering console.log
+};
 
