958e849a817603f3b54f8d5513ea071cd900bf08
dayannaold <180118889@aluno.unb.br>
2023-02-23 22:01:38 -0800

    complete

--- Snack Game/food.js
+++ Snack Game/food.js
@@ -1,12 +1,28 @@
-let food = { x: 0, y: 0}
+import { onSnake, expandSnake} from './snake.js'
+import { randomGridPosition} from './grid.js'
+
+let food = getRandomFoodPosition()
+const EXPANSION_RATE = 5
+
 export function update() {
- 
+    if (onSnake(food)){
+        expandSnake(EXPANSION_RATE)
+        food = getRandomFoodPosition() 
+    }
+
 }
 
 export function draw(gameBoard) {
-        const foodElement = document.createElement("div");
+        const foodElement = document.createElement('div')
         foodElement.style.gridRowStart = food.y
         foodElement.style.gridColumnStart = food.y
-        foodElement.classList.add("food")
+        foodElement.classList.add('food')
         gameBoard.appendChild(foodElement)
     }
+function getRandomFoodPosition() {
+    let newFoodPosition
+    while (newFoodPosition == null || onSnake(newFoodPosition)) {
+            newFoodPosition = randomGridPosition ()
+    }
+    return newFoodPosition
+}
--- Snack Game/game.js
+++ Snack Game/game.js
@@ -1,25 +1,38 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED} from './snake.js'
-import { update as updateFood, draw as drawFood} from './food.js'
-let lastRenderTime = 0;
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } 
+from './snake.js'
+import { update as updateFood, draw as drawFood}
+from './food.js'
+import { outsideGrid } from './grid.js'
+
+let lastRenderTime = 0
+let gameOver = false
 const gameBoard = document.getElementById ('game-board')
 
 function main(currentTime) {
-    window.requestAnimationFrame(main);
-    const secondSinceLastRender = (currentTime - lastRenderTime) / 1000;
-    if (secondSinceLastRender < 1 / SNAKE_SPEED) return;
+    if (gameOver) {
+       if (confirm('you lost. Press ok to restart')){
+        window.location = '/'
+       }
+       return
+    }
+
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
     
-    lastRenderTime = currentTime;
+lastRenderTime = currentTime
 
     update()
     draw()
     
 }
 
-window.requestAnimationFrame(main);
+window.requestAnimationFrame(main)
 
 function update(){
     updateSnake()
     updateFood()
+    checkDeath()
 
 }
 function draw(){
@@ -27,4 +40,7 @@ function draw(){
     drawSnake(gameBoard)
     drawFood(gameBoard)
 
+}
+function checkDeath(){
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection ()
 }
