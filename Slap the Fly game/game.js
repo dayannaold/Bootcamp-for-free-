e9628452539774465b6afb9b8a5148a63e835ad5
dayannaold <180118889@aluno.unb.br>
2023-04-04 20:14:23 -0700

    layout

--- Slap the Fly game/game.js
+++ Slap the Fly game/game.js
@@ -14,6 +14,12 @@ AdaptingSizeGame()
 
 function randomPosition() {
 
+if (document.getElementById('fly'))
+{
+document.getElementById('fly').remove()
+}
+
+
 	var posX = Math.floor(Math.random() * Height) - 90
 	var posY = Math.floor(Math.random() * Width) - 90
 
@@ -24,11 +30,50 @@ function randomPosition() {
 
 	var mosquito = document.createElement('img')
 	mosquito.src = 'imagens/mosquito.png'
-	mosquito.className = 'mosquito1'
+	mosquito.className = randomSize() + ' ' + randomSide()
 	mosquito.style.left = posX + 'px'
 	mosquito.style.top = posY + 'px'
 	mosquito.style.position = 'absolute'
+	mosquito.id = 'fly'
 
 	document.body.appendChild(mosquito)
+
 }
 
+function randomSize(){
+	var classe = Math.floor(Math.random() * 3)
+
+	switch(classe) {
+
+			case 0:
+				return 'fly1'
+
+			case 1:
+				return 'fly2'
+
+			case 2:
+				return 'fly3'
+	}
+}
+
+function randomSide(){
+	var classe = Math.floor(Math.random() * 2)
+
+	switch(classe) {
+
+			case 0:
+				return 'sideA'
+
+			case 1:
+				return 'sideB'
+
+			case 2:
+				return 'fly3'
+
+			case 3:
+				return 'sideB'
+
+			case 4:
+				return 'fly5'
+	}
+}
